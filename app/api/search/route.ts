859d38da1664272c0b9641cded71bb02a1192eb5
matthewguerra-a <mguerra5@illinois.edu>
2025-12-04 18:27:43 -0600

    created API call route

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -4,11 +4,23 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
+    const url = 'https://api.sunrise-sunset.org/json';
+    const lat = Number(searchParams.get('lat'));
+    const lng = Number(searchParams.get('lng'));
+    const date = searchParams.get('date') ?? ''; // defaults to today's date.
+    const formatted = 1; // formats times in ISO format.
+    const tzid = 'America/Chicago'; // times returned will be in Chicago timezone.
 
-    const location = searchParams.get('location');
-    console.log(location);
+    let query: string = `${url}?lat=${lat}&lng=${lng}`;
+    if (date) {
+      query += `&date=${date}`;
+    }
+    query += `&formatted=${formatted}&tzid=${tzid}`;
 
-    // fetch info from sunset api
+    const resp = await fetch(query);
+    const data = await resp.json();   
 
-    return NextResponse.json({  }, { status: 200 });
+    return NextResponse.json(data, { status: 200 });
 }
+
+
