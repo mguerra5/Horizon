4f0c63b350546c66a43cb95615c4fdc07c2cbd3d
GSDV <sdvgab@gmail.com>
2025-12-06 09:45:55 -0600

    Input changes

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,27 +1,40 @@
-interface FormInputProps {
+interface StringFormInput {
     type: 'email' | 'text' | 'password';
     placeholder: string;
     value: string;
     onChange: (v: string) => void;
 }
 
-export default function FormInput({
-    type,
-    placeholder,
-    value,
-    onChange
-}: FormInputProps) {
+interface NumberFormInput {
+    type: 'number';
+    placeholder: string;
+    value: number;
+    onChange: (v: number) => void;
+}
+
+type FormInputProps = StringFormInput | NumberFormInput;
+
+export default function FormInput(props: FormInputProps) {
     return (
         <input
-            type={type}
-            placeholder={placeholder}
+            type={props.type}
+            placeholder={props.placeholder}
             required
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
+            value={props.value}
+            onChange={(e) => {
+                if (props.type !== 'number') {
+                    props.onChange(e.target.value);
+                } else {
+                    props.onChange(Number(e.target.value));
+                }
+            }}
             style={{
                 padding: 10,
                 borderRadius: 10,
-                border: '1px solid black'
+                flex: '0 0 auto',
+                minWidth: 0,
+                border: '1px solid black',
+                width: '150px'
             }}
         />
     );
