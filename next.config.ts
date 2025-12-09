45eef92add958b103db2f598f984f4a1056737f4
GSDV <sdvgab@gmail.com>
2025-12-09 16:01:26 -0600

    correcting config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true, // <– disables ALL ESLint errors during build
-  },
 };
 
 export default nextConfig;
